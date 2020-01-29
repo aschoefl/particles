942912de1b67b3bc3d76e5c6b99a0e5924a9e9c7
selachii <amanda.schoefl@student.tuwien.ac.at>
2020-01-29 16:56:12 +0100

    more than one round is working

--- particle.py
+++ particle.py
@@ -1,3 +1,4 @@
+#!python3
 import sys, random
 import pymunk
 import numpy as np 
@@ -30,48 +31,67 @@ VELOCITY_MEAN_O3 = (4*k*T/MASS_OF_O3)**(1/2)*10**(3/2)
 VELOCITY_STD_O3 = (k*T/MASS_OF_NO*(2-(2**(3/2)-4))*constants.pi)**(1/2)*10**(3/2)  #(k*T/MASS_OF_O3*(4*constants.pi-2))**(1/2)
 REACTION_ENERGY = 1.93*10**6 # eigentlich 10^-20 J
 MAX_TIME = 1000
-MAX_ROUNDS = 30
+MAX_ROUNDS = 300
 DT = 0.001 # timestep
 
 amount_of_reactions = 0
 
 
-def addParticles(space, amount, molecule_type, existing_particles = []):
-    """Add a ball to the given space at a random position"""
-    if molecule_type == "NO":
-        mass = MASS_OF_NO
-        vel_mean = VELOCITY_MEAN_NO
-        vel_std = VELOCITY_STD_NO
-    elif molecule_type == "O3":
-        mass = MASS_OF_O3
-        vel_mean = VELOCITY_MEAN_O3
-        vel_std = VELOCITY_STD_O3
-    else:
-        assert("wrong input for molecule_type")
-    radius = RADIUS_OF_PARTICLES
+def addParticles(space, amount_list, molecule_type_list, existing_particles = []):
+    try:
+        amount_list = list(amount_list)
+    except:
+        tmp = amount_list
+        amount_list = []
+        amount_list.append[tmp]
+    try:
+        molecule_type_list = list(molecule_type_list)
+    except:
+        tmp = molecule_type_list
+        molecule_type_list = []
+        molecule_type_list.append[molecule_type_list]
 
+    assert len(molecule_type_list)==len(amount_list)
     particles = existing_particles
-    for j in range(0, amount):
-        inertia = pymunk.moment_for_circle(mass, 0, radius, (0,0))
-        body = pymunk.Body(mass, inertia)
-        p_abs = random.gauss(vel_mean, vel_std)*mass
-        x_dir = random.random()*random.choice([-1,1])
-        y_dir = (1-x_dir**2)**(1/2)*random.choice([-1,1])
-        body.apply_impulse_at_local_point((x_dir*p_abs, y_dir*p_abs))
-        x = random.uniform(radius/2+LINES_RADIUS/2+2,SPACE_SIZE-radius/2-LINES_RADIUS/2-2)
-        y = random.uniform(radius/2+LINES_RADIUS/2+2,SPACE_SIZE-radius/2-LINES_RADIUS/2-2)
-        body.position = x, y
-        shape = pymunk.Circle(body, radius, (0,0))
-        shape.elasticity = ELASTICITY
-        shape.friction = 0
-        shape.collision_type = 1
-        shape.molecule_type = molecule_type
-        space.add(body, shape)
-        try:
-            particles.append(body)
-        except:
-            "Wrong type for existing_particles in addParticles"
+    
+    for i in range(0, len(amount_list)):
+        molecule_type = molecule_type_list[i]
+        amount = amount_list[i]
+        """Add a ball to the given space at a random position"""
+        if molecule_type == "NO":
+            mass = MASS_OF_NO
+            vel_mean = VELOCITY_MEAN_NO
+            vel_std = VELOCITY_STD_NO
+        elif molecule_type == "O3":
+            mass = MASS_OF_O3
+            vel_mean = VELOCITY_MEAN_O3
+            vel_std = VELOCITY_STD_O3
+        else:
+            assert("wrong input for molecule_type")
+        radius = RADIUS_OF_PARTICLES
+
+        for j in range(0, amount):
+            inertia = pymunk.moment_for_circle(mass, 0, radius, (0,0))
+            body = pymunk.Body(mass, inertia)
+            p_abs = random.gauss(vel_mean, vel_std)*mass
+            x_dir = random.random()*random.choice([-1,1])
+            y_dir = (1-x_dir**2)**(1/2)*random.choice([-1,1])
+            body.apply_impulse_at_local_point((x_dir*p_abs, y_dir*p_abs))
+            x = random.uniform(radius/2+LINES_RADIUS/2+2,SPACE_SIZE-radius/2-LINES_RADIUS/2-2)
+            y = random.uniform(radius/2+LINES_RADIUS/2+2,SPACE_SIZE-radius/2-LINES_RADIUS/2-2)
+            body.position = x, y
+            shape = pymunk.Circle(body, radius, (0,0))
+            shape.elasticity = ELASTICITY
+            shape.friction = 0
+            shape.collision_type = 1
+            shape.molecule_type = molecule_type
+            space.add(body, shape)
+            try:
+                particles.append(body)
+            except:
+                "Wrong type for existing_particles in addParticles"
     return(particles)
+
 
 def addLines(space):
     body = pymunk.Body(body_type = pymunk.Body.STATIC)
@@ -106,7 +126,6 @@ def collision_handler(arbiter, space, data):
     global amount_of_reactions
     s = arbiter.shapes
     n = np.array(arbiter.contact_point_set.normal)
-
     for j in range(0, len(s)):
         if s[j].molecule_type in ["NO2, O2"]:
             pass
@@ -142,17 +161,13 @@ def main():
     ch.pre_solve = collision_handler
 
     addLines(space)
-
+    aor_list = np.zeros(MAX_ROUNDS)
     for r in range(1,MAX_ROUNDS):
-        particles = addParticles(space, AMOUNT_OF_NO, molecule_type="NO")
-        particles = addParticles(space, AMOUNT_OF_O3, molecule_type="O3", existing_particles=particles)
-        for p in particles:
-            space.remove(p.shape)        
-
+        particles = addParticles(space, amount_list=[AMOUNT_OF_NO, AMOUNT_OF_O3], molecule_type_list=["NO","O3"], existing_particles=[])
         if GRAPHICS:
             pygame.init()
             screen = pygame.display.set_mode((SPACE_SIZE, SPACE_SIZE))
-            pygame.display.set_caption("Ich will nicht Algebra lernen")
+            pygame.display.set_caption("particles in gas")
             clock = pygame.time.Clock()
 
             draw_options = pymunk.pygame_util.DrawOptions(screen)
@@ -175,7 +190,18 @@ def main():
         else:
             for time in range(0, MAX_TIME):
                 space.step(DT)
+        aor_list[r] = amount_of_reactions
         print("amount of reactions: {}".format(amount_of_reactions))
+        
+        for i in range (0,len(particles)):
+            space.remove(list(particles[i].shapes)[0].body, list(particles[i].shapes)[0])
+        particles=[]
+        
+        amount_of_reactions=0
+
+    print("mean of first half:", np.mean(aor_list[range(1, int(MAX_ROUNDS/2))]))
+    print("mean of second half:", np.mean(aor_list[range(int(MAX_ROUNDS/2), MAX_ROUNDS-1)]))
+
         
 
 if __name__ == '__main__':
