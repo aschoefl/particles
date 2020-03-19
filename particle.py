f165db7ce4a6cef40c3923c201931381001b06fd
selachii <amanda.schoefl@student.tuwien.ac.at>
2020-03-19 17:12:15 +0100

    add comments

--- particle.py
+++ particle.py
@@ -7,39 +7,63 @@ from scipy import constants
 import pygame
 from pygame.locals import *
 import pymunk.pygame_util
- 
 
+## graphic parameters
+GRAPHICS = True # True - graphic output (slow); False - no graphic output (faster)
+DT = 0.01        # timestap parameter for graphics
+
+## physical parameters
+T = 410             # [K]
+k = 1.38064852    # actually 1.38064852*10^-23 [J/K]
+AMOUNT_OF_NO = 500  
+AMOUNT_OF_O3 = 500
+RADIUS_OF_PARTICLES = 1   # actually *10^-10 m
+MASS_OF_NO = (2.3+2.67)/10**3  # actually *10^-26 kg
+MASS_OF_O3 = 2.67*3/10**3     # actually *10^-26 kg
+VELOCITY_MEAN_NO = (k*T*constants.pi/MASS_OF_NO/2)**(1/2)  
+VELOCITY_STD_NO = (k*T/MASS_OF_NO*(1-constants.pi/4))**(1/2)  
+VELOCITY_MEAN_O3 = (k*T*constants.pi/MASS_OF_O3/2)**(1/2)
+VELOCITY_STD_O3 = (k*T/MASS_OF_O3*(1-constants.pi/4))**(1/2)  
+THRESHOLD_ENERGY = 1.93*10**3  # actually 1,93*10^-20 J
+REACTION_ENERGY = 3.093*10**2  # actually 3,093*10^-19 J
+
+## model parameters
+MAX_TIME = 500      # time per round
+MAX_ROUNDS = 300    # amount of runs
+SPACE_SIZE = 1000  
+ELASTICITY = 0.999  # should not be >= 1 according to docu of pymunk
+LINES_RADIUS = 4 
+
+## set random seed
 random.seed = 42
 
-## collision_type: 0-lines, 1-particle
-
-## GLOBALS
-GRAPHICS = True
-T = 410 # Kelvin 273
-k = 1.38064852 # eigentlich 10^-23
-SPACE_SIZE = 1000
-ELASTICITY = 0.999 # should not be >= 1 according to docu of pymunk
-LINES_RADIUS = 4
-AMOUNT_OF_NO = 500
-AMOUNT_OF_O3 = 500
-RADIUS_OF_PARTICLES = 1 # eigentlich 10^-10 m
-MASS_OF_NO = (2.3+2.67)  # eigentlich 10^-26 kg
-MASS_OF_O3 = 2.67*3     # eigentlich 10^-26 kg
-VELOCITY_MEAN_NO = (4*k*T/MASS_OF_NO)**(1/2)*10**(3/2)
-VELOCITY_STD_NO = (k*T/MASS_OF_NO*(2-(2**(3/2)-4))*constants.pi)**(1/2)*10**(3/2)  #(k*T/MASS_OF_NO*(4*constants.pi-2))**(1/2)
-VELOCITY_MEAN_O3 = (4*k*T/MASS_OF_O3)**(1/2)*10**(3/2)
-VELOCITY_STD_O3 = (k*T/MASS_OF_O3*(2-(2**(3/2)-4))*constants.pi)**(1/2)*10**(3/2)  #(k*T/MASS_OF_O3*(4*constants.pi-2))**(1/2)
-
-THRESHOLD_ENERGY = 1.93*10**(-10) # eigentlich 10^-20 J
-REACTION_ENERGY = 3.283*10**(-9) # actually 3,283*10^-19 J
-MAX_TIME = 500
-MAX_ROUNDS = 300
-DT = 0.01 # timestep was 0.001
-
+## initiate global counter
 amount_of_reactions = 0
 
-
+##############################
+## 
+##############################
 def addParticles(space, amount_list, molecule_type_list, existing_particles = []):
+    """ 
+    function that adds particles for an existing space
+
+    Parameters:
+    -----------
+    space: space object as returned by pymunk.Space()
+    amount_list: list of int
+        containing the amount of particles to be produced for each
+        type of molecule in molecule_type_list
+    molecule_type_list: list of str
+        containig types of particles to be produced (allowed: "NO" and "O3")
+        in the same order as in amount_list
+    existing_particles: list of objects
+        list of all particles already existing in the space
+
+    Returns:
+    --------
+    list of all particles in the space. contains objects as
+    returned by pymunk.Body(.)
+    """
     try:
         amount_list = list(amount_list)
     except:
@@ -75,7 +99,10 @@ def addParticles(space, amount_list, molecule_type_list, existing_particles = []
         for j in range(0, amount):
             inertia = pymunk.moment_for_circle(mass, 0, radius, (0,0))
             body = pymunk.Body(mass, inertia)
+            ## set initial impuls and coordinates of particles
+            ## gaussian distribution of velocity
             p_abs = random.gauss(vel_mean, vel_std)*mass
+            ## uniform distribution of coordinates
             x_dir = random.random()*random.choice([-1,1])
             y_dir = (1-x_dir**2)**(1/2)*random.choice([-1,1])
             body.apply_impulse_at_local_point((x_dir*p_abs, y_dir*p_abs))
@@ -85,6 +112,7 @@ def addParticles(space, amount_list, molecule_type_list, existing_particles = []
             shape = pymunk.Circle(body, radius, (0,0))
             shape.elasticity = ELASTICITY
             shape.friction = 0
+            ## definition of collision_type: 0-lines, 1-particle
             shape.collision_type = 1
             shape.molecule_type = molecule_type
             space.add(body, shape)
@@ -96,6 +124,15 @@ def addParticles(space, amount_list, molecule_type_list, existing_particles = []
 
 
 def addLines(space):
+    """ 
+    function that draws a quadratic outline of size SPACE_SIZExSPACE_SIZE
+    
+    Parameters:
+    -----------
+    space: space object as returned by pymunk.Space()
+
+    """
+
     body = pymunk.Body(body_type = pymunk.Body.STATIC)
     body.position = (0,0)
     body.elasticity = ELASTICITY
@@ -111,66 +148,103 @@ def addLines(space):
     for l in lines:
         l.elasticity = ELASTICITY
         l.friction = 0
+        ## definition of collision_type: 0-lines, 1-particle
         l.collision_type = 0
-        
     space.add(lines[0], lines[1], lines[2], lines[3], body)
 
 def reaction(v1, v2, m1, m2):
-    if m1*m2*(v1-v2)**2/(2*(m1+m2)) >= THRESHOLD_ENERGY:
+    """ 
+    determines if relative kinetic energy is high enough
+    for a reaction to happen
+    
+    Parameters:
+    -----------
+    v1, v1: int
+        velocity of colliding objects
+    m1, m2: int
+        mass of colliding objects
+
+    Returns:
+    --------
+    True if collision happens, False else
+
+    """
+    if m1*m2*np.dot(np.array(v1-v2),np.array(v1-v2))/(2*(m1+m2)) >= THRESHOLD_ENERGY:
         return True
     else:
         return False
 
 def set_new_velocites(p1, p2):
-    # divide ekin by 2 beacause apparently s[j].body.kinetic_energy === np.linalg.norm(s[j].body.velocity)**2*s[j].body.mass
+    """ 
+    simulates a exothermic reaction by altering the particle's velocity
+
+    Parameters:
+    -----------
+    p1, p2: objects as returned by pymunk.Body(.)
+        colliding objects
+    """
+    # divide ekin by 2 beacause apparently:
+    # s[j].body.kinetic_energy === np.linalg.norm(s[j].body.velocity)**2*s[j].body.mass
     alpha = 1 + REACTION_ENERGY/(p1.body.kinetic_energy/2+p2.body.kinetic_energy/2)
     p1.body.velocity = p1.body.velocity*alpha
     p2.body.velocity = p2.body.velocity*alpha
     
 def collision_handler(arbiter, space, data):
+    """ 
+    pre solve collision handler (see pymunk documentation for details)
+    determines if a reaction between the colliding objects is happening
+
+    Parameters:
+    -----------
+    arbiter: pymunk arbiter object
+    space: space object as returned by pymunk.Space()
+    """
     global amount_of_reactions
     s = arbiter.shapes
-    n = np.array(arbiter.contact_point_set.normal)
+    #n = np.array(arbiter.contact_point_set.normal)
     for j in range(0, len(s)):
         if s[j].molecule_type in ["NO2, O2"]:
             pass
         elif s[j].molecule_type == "NO":
             for i in range(j+1, len(s)):
-                if s[i].molecule_type == "O3" and reaction(np.dot(np.array(s[j].body.velocity),n), np.dot(np.array(s[i].body.velocity),n),
+                if s[i].molecule_type == "O3" and reaction(s[j].body.velocity, s[i].body.velocity,
                             s[j].body.mass, s[i].body.mass):
+                    ## reaction happens
                     s[j].molecule_type = "NO2"
                     s[i].molecule_type = "O2"
                     set_new_velocites(s[i],s[j])
                     amount_of_reactions += 1
-                    #print("reaction")
                     break
         elif s[j].molecule_type == "O3":
             for i in range(j+1, len(s)):
-                if s[i].molecule_type == "NO" and reaction(np.dot(np.array(s[j].body.velocity),n), np.dot(np.array(s[i].body.velocity),n),
+                if s[i].molecule_type == "NO" and reaction(s[j].body.velocity, s[i].body.velocity,
                             s[j].body.mass, s[i].body.mass):
+                    ## reaction happens
                     s[j].molecule_type = "O2"
                     s[i].molecule_type = "NO2"
                     set_new_velocites(s[i],s[j])
                     amount_of_reactions += 1
-                    #print("reaction")
                     break
-                
-    pass
     return True
 
 
 def main():
     global amount_of_reactions
 
+    ## construct space
     space = pymunk.Space()
     space.gravity = (0.0,0.0)
     ch = space.add_collision_handler(1, 1)
     ch.pre_solve = collision_handler
-
     addLines(space)
+
+    ## list storing the amount of reactions for each round
     aor_list = np.zeros(MAX_ROUNDS)
     for r in range(1,MAX_ROUNDS):
-        particles = addParticles(space, amount_list=[AMOUNT_OF_NO, AMOUNT_OF_O3], molecule_type_list=["NO","O3"], existing_particles=[])
+        ## add new particles
+        particles = addParticles(space, amount_list=[AMOUNT_OF_NO, AMOUNT_OF_O3], 
+            molecule_type_list=["NO","O3"], existing_particles=[])
+        ## display particles if GRAPHICS==True
         if GRAPHICS:
             pygame.init()
             screen = pygame.display.set_mode((SPACE_SIZE, SPACE_SIZE))
@@ -190,20 +264,22 @@ def main():
 
                 space.debug_draw(draw_options)
 
-                space.step(DT) # was 1./50
+                space.step(DT)
 
                 pygame.display.flip()
                 clock.tick(1./DT)
         else:
             for time in range(0, MAX_TIME):
                 space.step(DT)
+
+        ## store and print amount of reactions in current round
         aor_list[r] = amount_of_reactions
         print("amount of reactions: {}".format(amount_of_reactions))
         
+        ## delete all particles and reset amount of reactions
         for i in range (0,len(particles)):
             space.remove(list(particles[i].shapes)[0].body, list(particles[i].shapes)[0])
         particles=[]
-        
         amount_of_reactions=0
 
     print("mean of first half:", np.mean(aor_list[range(1, int(MAX_ROUNDS/2))]))
